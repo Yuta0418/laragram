0de6202114d4f712c2952fcc4db57a0311052641
EC2 Default User <ec2-user@ip-172-31-70-237.ec2.internal>
2022-02-28 11:03:37 +0000

    fix how to show images

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -48,8 +48,7 @@ class UsersController extends Controller
         $user = User::find($request->id);
         $user->name = $request->user_name;
         if ($request->user_profile_photo !=null) {
-            $request->user_profile_photo->storeAs('public/user_images', $user->id . '.jpg');
-            $user->profile_photo = $user->id . '.jpg';
+            $user->image = base64_encode(file_get_contents($request->user_profile_photo));
         }
         $user->password = bcrypt($request->user_password);
         $user->save();
